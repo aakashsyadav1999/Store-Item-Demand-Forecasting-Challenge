1fe33889e0e48d4fedd4adf47c183015b04275db
Aakash Yadav <aakashsyadav1999@gmail.com>
2024-05-12 13:06:40 +0530

    __init__

--- src/store_demand_forecasting/constants/__init__.py
+++ src/store_demand_forecasting/constants/__init__.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 
-ARTIFACTS_DIR = os.path.join("artifacts", TIMESTAMP)
+ARTIFACTS_DIR = os.path.join("artifacts")
 LOGS_DIR = "logs"
 LOGS_FILE_NAME = "SIDFC.log"
 MODELS_DIR = "models"
@@ -14,3 +14,8 @@ ROOT_DIR= "DataIngestionArtifacts"
 SOURCE_URL= "https://github.com/aakashsyadav1999/Store-Item-Demand-Forecasting-Challenge/blob/main/research/demand-forecasting-kernels-only.zip"
 UNZIP_DIR = ROOT_DIR
 LOCAL_FILE_PATH = "demand-forecasting-kernels-only.csv"
+
+#Data Transformation
+DATA_TRANSFORMATION_DIR = "DataTransformationArtifacts"
+TRANSFORMED_FILE_NAME = 'final.csv'
+CSV_FILE_NAME = r'D:\VS code files\store_item_demand_forecasting_challenge\artifacts\DataIngestionArtifacts\train.csv'
