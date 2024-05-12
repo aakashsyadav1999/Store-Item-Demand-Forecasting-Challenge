4ef40cd2638bed82487694326b5e24e4f44cc4bf
Aakash Yadav <aakashsyadav1999@gmail.com>
2024-05-12 13:06:15 +0530

    added_data_transformation

--- src/store_demand_forecasting/entity/config_entity.py
+++ src/store_demand_forecasting/entity/config_entity.py
@@ -5,10 +5,12 @@ import sys
 from src.store_demand_forecasting.constants import *
 from src.store_demand_forecasting.entity.artifact_entity import *
 from src.store_demand_forecasting.entity.config_entity import *
+
+
 #Data Ingestion
 @dataclass
 class DataIngestionConfig:
-    def __init__(self):
+    def __init__ (self):
         self.data_ingestion_artifacts_dir:str = os.path.join(
             ARTIFACTS_DIR,ROOT_DIR
         )
@@ -19,4 +21,15 @@ class DataIngestionConfig:
         self.unzip_dir:str = os.path.join(
             self.data_ingestion_artifacts_dir
         )
+#Data Transformation
+@dataclass
+class DataTransformationConfig:
+    def __init__ (self):
+        self.data_transformation_dir:str = os.path.join(
+            ARTIFACTS_DIR,DATA_TRANSFORMATION_DIR
+        )
+        self.final_file_name: str = os.path.join(
+            self.data_transformation_dir,TRANSFORMED_FILE_NAME
+        )
+        self.read_csv:str = CSV_FILE_NAME
 
