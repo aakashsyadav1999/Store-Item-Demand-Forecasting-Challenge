af21d52292933aad7596ba99f44693cb240d7345
Aakash Yadav <aakashsyadav1999@gmail.com>
2024-05-24 22:51:06 +0530

    constant_file_changes

--- src/store_demand_forecasting/constants/__init__.py
+++ src/store_demand_forecasting/constants/__init__.py
@@ -11,14 +11,14 @@ BEST_MODEL_DIR = "best_model"
 
 # Data Ingestion 
 ROOT_DIR= "DataIngestionArtifacts"
-SOURCE_URL= "https://github.com/aakashsyadav1999/Store-Item-Demand-Forecasting-Challenge/blob/main/research/demand-forecasting-kernels-only.zip"
-UNZIP_DIR = ROOT_DIR
-LOCAL_FILE_PATH = "demand-forecasting-kernels-only.csv"
+SOURCE_URL= "https://drive.google.com/file/d/12TOieo_IuboUIG25GmKjKh0G2q3UfBXA/view?usp=sharing"
+UNZIP_DIR= ROOT_DIR
+LOCAL_FILE_PATH = "sample_submission.zip"
 
 #Data Transformation
 DATA_TRANSFORMATION_DIR = "DataTransformationArtifacts"
 TRANSFORMED_FILE_NAME = 'final.csv'
-CSV_FILE_NAME = r'D:\VS code files\store_item_demand_forecasting_challenge\artifacts\DataIngestionArtifacts\train.csv'
+CSV_FILE_NAME = r'artifacts\DataTransformationArtifacts\train.csv'
 
 #Model Trainer
 LGB_PARAMS = {
